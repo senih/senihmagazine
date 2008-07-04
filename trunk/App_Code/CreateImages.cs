2b84d05c3df90e628f532ef12895155ed16224c1
vsenih <vsenih@b46b5ca2-3451-0410-b025-0fa55ad08bfa>
2008-07-04 15:52:54 +0000

    04.07.2008 Initial upload

---
+++ trunk/App_Code/CreateImages.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using UDCWRAPPERLib;
+using System.Diagnostics;
+
+namespace Magazine
+{
+    public class CreateImages
+    {
+        public static string CreateImagesMethod(string inputfile)
+        {
+            string status = "Error";
+            UDCWRAPPERLib.Printer PDFPrinter = new UDCWRAPPERLib.Printer();
+            UDCWRAPPERLib.Profile profile;
+            Acrobat.AcroApp app;
+            Acrobat.AcroAVDoc doc;
+            Acrobat.AcroPDDoc document;
+
+            try
+            {
+                PDFPrinter.SetAsDefaultPrinter();
+                PDFPrinter.ImportProfileFromFile(HttpContext.Current.Server.MapPath("~/profile/myprofile.udc"));
+                string pro = PDFPrinter.DefaultProfile;
+                profile = (UDCWRAPPERLib.Profile)PDFPrinter.get_Profile("MyProfile");
+                profile.Default = 1;
+                pro = PDFPrinter.DefaultProfile;
+                profile.PreDefinedImageFilePath = HttpContext.Current.Server.MapPath("~/pages");
+
+                //string pro = PDFPrinter.DefaultProfile;
+                //profile = (UDCWRAPPERLib.Profile) PDFPrinter.get_Profile(pro);
+                //string printpath = HttpContext.Current.Server.MapPath("~/pages");
+                //profile.name = "MyProfile";
+                //profile.PreDefinedImageFilePath = printpath;
+                //profile.ImageFileFormat = UDCWRAPPERLib.IMAGE_FORMAT.FMT_JPEG;
+                //profile.PreDefinedImageFileName = "&[Page No].&[Image Type]";
+                //int quality = 100;
+                //int depth = 24;
+                //profile.set_JPEGQuality(UDCWRAPPERLib.IMAGE_FORMAT.FMT_JPEG, quality);
+                //profile.set_ImageFileColorDepth(UDCWRAPPERLib.IMAGE_FORMAT.FMT_JPEG, depth);
+                //profile.PrintQualityX = 300;
+                //profile.PrintQualityY = 300;
+                //profile.PageWidth = 2657;
+                //profile.PageHeight = 3425;
+                //profile.PageOrientation = UDCWRAPPERLib.PAGE_ORIENTATION.PO_PORTRAIT;
+                //profile.PostPrintAction = UDCWRAPPERLib.POST_PRINT_ACTION.PP_NONE;
+                //profile.Default = 1;
+                //profile.ExportToFile(HttpContext.Current.Server.MapPath("~/profile/myprofile.udc"));
+
+                //PDFPrinter.SetAsDefaultPrinter();
+                //PDFPrinter.ImportProfileFromFile(HttpContext.Current.Server.MapPath("~/profile/myprofile.udc"));
+                
+
+                app = (Acrobat.AcroApp)Microsoft.VisualBasic.Interaction.CreateObject("AcroExch.App", "");
+                doc = (Acrobat.AcroAVDoc)Microsoft.VisualBasic.Interaction.CreateObject("AcroExch.AVDoc", "");
+
+                bool temp = doc.Open(inputfile, "");
+                if (!temp)
+                {
+                    throw new FileNotFoundException("PDF file not found!");
+                }
+                document = (Acrobat.AcroPDDoc)doc.GetPDDoc();
+                int pagesNumber = document.GetNumPages();                
+                doc.PrintPagesSilent(0, pagesNumber, -1, 0, 0);
+                doc.Close(1);
+                document.Close();
+                app.Exit();
+
+                Marshal.ReleaseComObject(document);
+                Marshal.ReleaseComObject(doc);
+                Marshal.ReleaseComObject(app);
+                PDFPrinter.RemoveProfile("MyProfile");
+                                
+                status = "Image files created!";
+            }
+            catch (System.Exception ex)
+            {
+                status = ex.ToString();
+            }
+
+            return status;
+        }
+
+        public static void ResizeImages(string file)
+        {
+            Image image = Image.FromFile(file);
+            int width = image.Size.Width;
+            int height = image.Size.Height;
+            width = width / 2;
+            height = height / 2;
+            Image newimage = new Bitmap(width, height);
+            Graphics gr = Graphics.FromImage(newimage);
+            gr.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            gr.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            gr.SmoothingMode = SmoothingMode.HighQuality;
+            gr.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            gr.DrawImage(image, 0, 0, width, height);
+            ImageCodecInfo[] info = ImageCodecInfo.GetImageEncoders();
+            EncoderParameters param = new EncoderParameters(1);
+            int quality = 90;
+            param.Param[0] = new EncoderParameter(Encoder.Quality, quality);
+           
+            image.Dispose();
+            if (File.Exists(file))
+                File.Delete(file);
+            FileStream fs = File.Create(file);
+                        
+            newimage.Save(fs, info[1], param);
+            param.Dispose();
+            newimage.Dispose();
+        }
+
+        public static string CreateImagesGhostScript(string inputfile)
+        {
+            string app = ConfigurationManager.AppSettings["gspath"] + "gswin32";
+            string pdf = inputfile;
+            string output = HttpContext.Current.Server.MapPath("~/pages") + @"\%d.jpg ";
+            string args = @" -sDEVICE=jpeg -r150 -dJPEGQ=80 -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -dUseCIEColor=true -o " + output + inputfile;
+            ProcessStartInfo procInfo = new ProcessStartInfo(app, args);
+            procInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            procInfo.RedirectStandardOutput = true;
+            procInfo.RedirectStandardInput = true;
+            procInfo.RedirectStandardError = true;
+            procInfo.UseShellExecute = false;
+
+            Process createImages = Process.Start(procInfo);
+            StreamReader reader = createImages.StandardOutput;
+            string temp = reader.ReadLine();
+            reader.Close();
+            string status = "Image files created!";
+            return status;
+        }
+    }
+}
---
+++ trunk/App_Code/CreateXML.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web;
+using System.IO;
+using System.Xml;
+using System.Text;
+
+namespace Magazine
+{
+    public class CreateXML
+    {
+        public static void CreatePagesXMLFile()
+        {
+            string[] files = Directory.GetFiles(HttpContext.Current.Server.MapPath("~/pages"), "*.jpg");
+            XmlDocument pages = new XmlDocument();
+            StringBuilder node = new StringBuilder();
+
+            node.Append("<content width=\"1329\" height=\"1713\" bgcolor=\"cccccc\" loadercolor=\"ffffff\" panelcolor=\"5d5d61\" buttoncolor=\"5d5d61\" textcolor=\"ffffff\">");
+            for (int i=1; i<=files.Length; i++)
+            {
+                node.Append(string.Format("	<page src=\"pages/{0}.jpg\"/>", i));
+            }
+            node.Append("</content>");
+
+            pages.LoadXml(node.ToString());
+            pages.Save(HttpContext.Current.Server.MapPath("~/xml/Pages.xml"));
+        }
+    }
+}

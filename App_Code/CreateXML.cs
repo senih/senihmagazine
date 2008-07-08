f3f39e2dd7aa175ec2f72009bfce80de367c0577
vsenih <vsenih@b46b5ca2-3451-0410-b025-0fa55ad08bfa>
2008-07-08 16:07:10 +0000

    08.07.2008 Client version

--- App_Code/CreateXML.cs
+++ App_Code/CreateXML.cs
@@ -3,21 +3,27 @@ using System.Web;
 using System.IO;
 using System.Xml;
 using System.Text;
+using System.Drawing.Imaging;
+using System.Drawing;
 
 namespace Magazine
 {
     public class CreateXML
     {
-        public static void CreatePagesXMLFile()
+        public static void CreatePagesXMLFile(string path)
         {
-            string[] files = Directory.GetFiles(HttpContext.Current.Server.MapPath("~/pages"), "*.jpg");
+            string[] files = Directory.GetFiles(HttpContext.Current.Server.MapPath("~/content/" + path), "*.jpg");
+            Image image = Image.FromFile(files[0]);
+            int width = image.Size.Width;
+            int height = image.Size.Height;
+            image.Dispose();
             XmlDocument pages = new XmlDocument();
             StringBuilder node = new StringBuilder();
 
-            node.Append("<content width=\"1329\" height=\"1713\" bgcolor=\"cccccc\" loadercolor=\"ffffff\" panelcolor=\"5d5d61\" buttoncolor=\"5d5d61\" textcolor=\"ffffff\">");
+            node.Append(string.Format("<content width=\"{0}\" height=\"{1}\" bgcolor=\"666159\" loadercolor=\"ffffff\" panelcolor=\"5d5d61\" buttoncolor=\"5d5d61\" textcolor=\"ffffff\">", width, height));
             for (int i=1; i<=files.Length; i++)
             {
-                node.Append(string.Format("	<page src=\"pages/{0}.jpg\"/>", i));
+                node.Append(string.Format("	<page src=\"content/{0}/{1}.jpg\"/>", path, i));
             }
             node.Append("</content>");
 
---
+++ App_Code/Issue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace Magazine
+{
+    public class Issue
+    {
+        protected string _issueID;
+        protected string _issueDirectory;
+        protected string _height;
+        protected string _resolution;
+        protected string _quality;
+        protected string _txtAntialiasing;
+        protected string _graphAntialiasing;
+
+        public string IssueId
+        {
+            get { return _issueID; }
+            set { _issueID = value; }
+        }
+
+        public string IssueDirectory
+        {
+            get { return _issueDirectory; }
+            set { _issueDirectory = value; }
+        }
+
+        public string Resolution
+        {
+            get { return _resolution; }
+            set { _resolution = value; }
+        }
+
+        public string Quality
+        {
+            get { return _quality; }
+            set { _quality = value; }
+        }
+
+        public string TextAntialiasing
+        {
+            get { return _txtAntialiasing; }
+            set { _txtAntialiasing = value; }
+        }
+
+        public string GraphicsAntialiasing
+        {
+            get { return _graphAntialiasing; }
+            set { _graphAntialiasing = value; }
+        }
+
+        public Issue()
+        {
+        }
+    }
+}
